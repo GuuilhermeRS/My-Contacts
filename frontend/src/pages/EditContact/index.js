7572ba15a89a9f0abf0bc5c63e04470db30927e5
GuilhermeRS <guilherme.gke@gmail.com>
2022-04-08 10:23:43 -0300

    Criando componente ContactForm

--- frontend/src/pages/EditContact/index.js
+++ frontend/src/pages/EditContact/index.js
@@ -1,9 +1,16 @@
+import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
 
 export default function EditContact() {
   return (
-    <PageHeader
-      title="Editar contato"
-    />
+    <>
+      <PageHeader
+        title="Editar contato"
+      />
+
+      <ContactForm
+        buttonLabel="Salvar alterações"
+      />
+    </>
   );
 }
