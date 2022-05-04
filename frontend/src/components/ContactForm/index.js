3e4375f8118b6d3e023cd08271e78cbe90696ac3
GuilhermeRS <guilherme.gke@gmail.com>
2022-05-04 11:03:12 -0300

    Exibindo Erros na Interface

--- frontend/src/components/ContactForm/index.js
+++ frontend/src/components/ContactForm/index.js
@@ -56,7 +56,11 @@ export default function ContactForm({ buttonLabel }) {
     }
   }
 
-  // console.log(errors);
+  function getErrorMessageByFieldName(fieldName) {
+    return errors.find((error) => error.field === fieldName)?.message;
+  }
+
+  console.log(getErrorMessageByFieldName('name'));
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -68,16 +72,18 @@ export default function ContactForm({ buttonLabel }) {
 
   return (
     <Form onSubmit={handleSubmit}>
-      <FormGroup>
+      <FormGroup error={getErrorMessageByFieldName('name')}>
         <Input
+          error={getErrorMessageByFieldName('name')}
           placeholder="Nome"
           value={name}
           onChange={handleNameChange}
         />
       </FormGroup>
 
-      <FormGroup>
+      <FormGroup error={getErrorMessageByFieldName('email')}>
         <Input
+          error={getErrorMessageByFieldName('email')}
           placeholder="E-mail"
           value={email}
           onChange={handleEmailChange}
